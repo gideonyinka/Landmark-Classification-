5f5f5edcd1f7262c84d05ab98dbf6b84d89c9a12
Yinka Okunola <55193700+gideonyinka@users.noreply.github.com>
2023-11-30 22:28:37 +0100

    Add files via upload

---
+++ model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+
+
+# define the CNN architecture
+class MyModel(nn.Module):
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
+
+        super(MyModel, self).__init__()
+
+        # YOUR CODE HERE
+        # Define a CNN architecture. Remember to use the variable num_classes
+        # to size appropriately the output of your classifier, and if you use
+        # the Dropout layer, use the variable "dropout" to indicate how much
+        # to use (like nn.Dropout(p=dropout))
+        
+        # Convolutional layer 1
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2, 2)
+        
+        # Convolutional layer 2
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2, 2)
+        
+        # Convolutional layer 3
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2,2)
+        
+        # Flatten the layers
+        self.flatten = nn.Flatten()
+        
+        # Pass the flatten layers through the MLP
+        self.fc1 = nn.Linear(64*28*28, 500)
+        self.drp1 = nn.Dropout(p=dropout)
+        self.rel1 = nn.ReLU()
+        
+        # Linear Layer
+        self.fc2 = nn.Linear(500, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # YOUR CODE HERE: process the input tensor through the
+        # feature extractor, the pooling and the final linear
+        # layers (if appropriate for the architecture chosen)
+        
+        x = self.relu1(self.pool1(self.conv1(x)))
+        x = self.relu2(self.pool2(self.conv2(x)))
+        x = self.relu3(self.pool3(self.conv3(x)))
+        
+        x = self.flatten(x)
+        x = self.rel1(self.drp1(self.fc1(x)))
+        
+        x = self.fc2(x)
+        
+        return x
+
+
+######################################################################################
+#                                     TESTS
+######################################################################################
+import pytest
+
+
+@pytest.fixture(scope="session")
+def data_loaders():
+    from .data import get_data_loaders
+
+    return get_data_loaders(batch_size=2)
+
+
+def test_model_construction(data_loaders):
+
+    model = MyModel(num_classes=23, dropout=0.3)
+
+    dataiter = iter(data_loaders["train"])
+    images, labels = dataiter.next()
+
+    out = model(images)
+
+    assert isinstance(
+        out, torch.Tensor
+    ), "The output of the .forward method should be a Tensor of size ([batch_size], [n_classes])"
+
+    assert out.shape == torch.Size(
+        [2, 23]
+    ), f"Expected an output tensor of size (2, 23), got {out.shape}"
